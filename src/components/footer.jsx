5ff13e94fd9eef550ac2437a1cde048a6ff53dc2
MateenSohail-coder <abdulmateensohailking@gmail.com>
2025-08-27 12:33:48 +0500

    clear all

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-  <footer className='bg-black text-center text-neutral-400 fixed bottom-0 w-full text-sm p-1.5 '>
-    <div className="content">&copy; 2025 TaskWriter copyright.All rigth reserve</div>
-  </footer>
-  )
-}
